4ce3851d13432d84364c9a5ee6f50415f8485dac
Jan Skwarczeński <skwarko@student.agh.edu.pl>
2023-08-01 20:40:20 +0200

    Login and registration (with admin/user gate)

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Enums\UserRole;
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +24,19 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->defineUserRoleGate('isAdmin',UserRole::ADMIN);
+        $this->defineUserRoleGate('isUser',UserRole::USER);
+    }
+
+    /**
+     * @param string $name
+     * @param string $role
+     * @return void
+     */
+    private function defineUserRoleGate(string $name, string $role): void
+    {
+        Gate::define($name, function (User $user) use ($role) {
+            return $user->role == $role;
+        });
     }
 }

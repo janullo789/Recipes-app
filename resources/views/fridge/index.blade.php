1cbd4efe24d185d958744fb9401211791ce6c1e5
Jan Skwarczeński <skwarko@student.agh.edu.pl>
2023-10-24 21:27:49 +0200

    Improve templates of my fridge

--- resources/views/fridge/index.blade.php
+++ resources/views/fridge/index.blade.php
@@ -6,35 +6,40 @@
     </x-slot>
 
     <div class="py-12 bg-gray-100">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-lg rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-300">
-                    <form method="POST" action="{{ route('fridge.store') }}">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="bg-white rounded-lg shadow-lg">
+                <div class="p-6">
+                    <form method="POST" action="{{ route('fridge.store') }}" class="space-y-6">
                         @csrf
                         @foreach ($ingredients as $category => $ingredientsGroup)
-                            <h2 class="text-2xl">{{ $category }}</h2>
-                            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                                @foreach ($ingredientsGroup as $ingredient)
-                                    <div class="flex flex-col rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300">
-                                        <div class="flex-1 bg-white p-6 flex flex-col justify-between">
-                                            <div class="flex-1">
-                                                <p class="text-xl font-semibold text-gray-900">
-                                                    {{ $ingredient->name }}
-                                                </p>
-                                                <input type="number"
-                                                       name="quantity[{{ $ingredient->id }}]"
-                                                       value="{{ $user_ingredients[$ingredient->id]->quantity ?? '' }}">
-                                            </div>
+                            <div>
+                                <h2 class="text-lg mb-4 flex items-center">
+                                    <span class="flex-grow border-t border-gray-300"></span>
+                                    <span class="px-2 text-center">
+                                        {{ ucfirst($category) }}
+                                    </span>
+                                    <span class="flex-grow border-t border-gray-300"></span>
+                                </h2>
+                                <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+                                    @foreach ($ingredientsGroup as $ingredient)
+                                        <div class="flex flex-col p-2 rounded-lg shadow hover:shadow-md transition ease-in-out duration-150
+                                                {{ isset($user_ingredients[$ingredient->id]) ? 'bg-green-100' : 'bg-white' }}">
+                                            <p class="text-md font-medium mb-1">{{ $ingredient->name }}</p>
+                                            <input type="number"
+                                                   name="quantity[{{ $ingredient->id }}]"
+                                                   value="{{ $user_ingredients[$ingredient->id]->quantity ?? '' }}"
+                                                   class="border rounded p-1 w-full text-sm">
                                         </div>
-                                    </div>
-                                @endforeach
+                                    @endforeach
+                                </div>
                             </div>
                         @endforeach
-                        <button type="submit" class="mt-4 px-4 py-2 bg-indigo-700 text-white rounded">Zapisz</button>
+                        <button type="submit" class="mt-4 w-full sm:w-auto bg-indigo-700 text-white py-2 px-4 rounded hover:bg-indigo-600">
+                            Zapisz
+                        </button>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-
 </x-app-layout>

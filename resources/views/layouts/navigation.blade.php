6c8711cf9b8580f2239ce33d993a0d03ae137d2e
Jan Skwarczeński <skwarko@student.agh.edu.pl>
2023-10-28 23:56:41 +0200

    Responsive menu nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -115,7 +115,12 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('mainSite')" :active="request()->routeIs('mainSite')">
-                {{ __('Main site') }}
+                {{ __('Przepisy') }}
+            </x-responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('fridge.index')" :active="request()->routeIs('fridge.index')">
+                {{ __('Twoja lodówka') }}
             </x-responsive-nav-link>
         </div>
 
